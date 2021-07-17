e4acb962c6df4dd45aeb48f3b885e1ccc49129cc
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-16 22:11:57 -0700

    it will add links in the html

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,14 @@ function Pizza(topping, size){
   this.size = size;
 }
 let pizza1 = new Pizza("pepperoni", "large");
+let small= 3;
+let medium = 4;
+let large = 5;
+
+Pizza.prototype.price = function(){
+  if (this.topping.includes("large")){
+    return large;
+    console.log(this.topping); 
+  }
+  return NaN;
+};
