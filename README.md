cdfd1e7c67490da6ca1aad2afa75f8c3f6ccd02f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-18 06:14:32 -0700

    add cost prototype

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Describe: Pizza ()
 Test: "it should create an object for pizza"
 Code: let myPizza = new Pizza ("pepperoni","large");
 myPizza.topping;
-Expected Output:pepperoni
+Expected Output: pepperoni
+
+Describe: Pizza.prototype.price = function()
+Test: "it should return 5 for the price"
+Code: pizza1;
+Expected Output : 5
--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,31 @@
 function Pizza(topping, size){
   this.topping = topping;
   this.size = size;
+  this.price= 0
 }
 let pizza1 = new Pizza("pepperoni", "large");
 let small= 3;
 let medium = 4;
 let large = 5;
 
-Pizza.prototype.price = function(){
-  if (this.topping.includes("large")){
-    return large;
-    console.log(this.topping); 
+Pizza.prototype.cost = function(){
+  if (this.size.includes('large')){
+     this.price = 5;
   }
-  return NaN;
+  else if (this.size.includes('medium')){
+    this.price = 4;
+  }
+  else if (this.size.includes('small')){
+    this.price = 3;
+  }
+  if (this.topping > 4){
+    this.price += 5;
+  }
+  else if (this.topping < 2){
+    this.price;
+  }
+  else {
+    this.price +=2;
+  }
 };
+
